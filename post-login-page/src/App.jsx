ca57db60b3443ddad0793474dfed69b803afd985
Fardin <syedfardin0612@gmail.com>
2025-09-22 19:25:49 +0530

    Driver finding and displaying frontend done; Next, make server side backend to send best drivers instead.

--- post-login-page/src/App.jsx
+++ post-login-page/src/App.jsx
@@ -1,17 +1,79 @@
 import 'leaflet/dist/leaflet.css';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, Marker, TileLayer, Popup } from 'react-leaflet';
 import { useEffect, useState, useRef } from 'react';
 import { Loader2 } from 'lucide-react';
-const carLender = [12.924865, 77.550866];
-const carPooler = [12.932375989090675, 77.55149747184085];
-const destination = [12.9350833, 77.5308973];
+// import DriverInfoCard from '../components/DriverInfoCard.jsx';
+import DriverInfoCard from '../components/driverInfoCard';
+
+
+try {
+  const socket = io('http://localhost:3000');
+}
+catch {
+  console.log("Error connecting to server!")
+}
+// const carLender = [12.924865, 77.550866];
+// const carPooler = [12.932375989090675, 77.55149747184085];
+// const destination = [12.9350833, 77.5308973];
 
 function App() {
+  const pesu_coord = [12.935434549896387, 77.53599357316021];
+  const someDrivers = [{
+    "name": "Alex",
+    "rating": "4.9",
+    "distance": "0.5 mi",
+    "eta": "3 min",
+  },
+  {
+    "name": "Ben",
+    "rating": "4.7",
+    "distance": "1.2 mi",
+    "eta": "7 min"
+  },
+  {
+    "name": "Chris",
+    "rating": "4.8",
+    "distance": "0.8 mi",
+    "eta": "5 min"
+  },
+  {
+    "name": "David",
+    "rating": "5.0",
+    "distance": "2.1 mi",
+    "eta": "10 min"
+  },
+  {
+    "name": "Ethan",
+    "rating": "4.6",
+    "distance": "0.6 mi",
+    "eta": "4 min"
+  }];
+
   const [currLocation, setCurrLocation] = useState({ lat: '', lng: '', place_name: '' });
-  const [destinationText, setDestinationText] = useState(destination);
+  const [destination, setDestination] = useState({ lat: '', lng: '', place_name: '' });
   const [error, setError] = useState(null);
   const [pageLoading, setPageLoading] = useState(true);
   const debounceTimer = useRef(null);
+  const [drivers, setDrivers] = useState([]);
+
+
+  const handleDestChange = (e) => {
+    setDestination({ lat: destination.lat, lng: destination.lng, place_name: e.target.value });
+  }
+
+  const pesuBtn = () => {
+    setDestination({ lat: pesu_coord[0], lng: pesu_coord[1], place_name: "PES University" });
+  }
+
+  const findDriver = () => {
+
+    //  Send API request to backend with currLocation and destination
+    //  Display top 5 drivers around rider, along with their ratings and everything
+
+    //Appending raw data now:
+    setDrivers(someDrivers);
+
+  }
 
   useEffect(() => {
     const getLocation = () => {
@@ -63,7 +125,7 @@ function App() {
       } finally {
         setPageLoading(false)
       }
-    }, 3000); // 3-second debounce
+    }, 5000); // 3-second debounce
   }, [currLocation.lat, currLocation.lng]);
 
   let inputStyle = {
@@ -163,27 +225,66 @@ function App() {
   }
 
   return (
-    <div
-      style={{
-        minHeight: '100vh',
-        minWidth: '100vw',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        flexDirection: 'column',
-      }}
-    >
-      <div>
-        Current Location: <input type="text" value={currLocation.place_name} readOnly style={inputStyle} />
+    <>
+
+      <div
+        style={{
+          minHeight: '100vh',
+          minWidth: '100vw',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          flexDirection: 'column',
+        }}
+      >
+        <div>
+          Current Location: <input type="text" value={currLocation.place_name} readOnly style={inputStyle} />
+        </div>
+        <br />
+        <div>
+          Destination: <input type="text" value={destination.place_name} onChange={handleDestChange} style={inputStyle} />
+          <button onClick={pesuBtn}>PESU</button>
+          <button onClick={findDriver}>Find Driver!</button>
+        </div>
+        <br />
+
+        <div style={{
+          backgroundColor: "red",
+          display: "flex"
+        }}>
+
+
+
+          <MapContainer center={[currLocation.lat, currLocation.lng]} zoom={14} style={{ height: '500px', width: '500px' }}>
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[currLocation.lat, currLocation.lng]}> <Popup>Your Location.</Popup> </Marker>
+            <Marker position={[destination.lat, destination.lng]}> <Popup>Destination</Popup></Marker>
+          </MapContainer>
+
+        </div>
+        <br />
+
+
+
+
+        <div style={{
+          marginBottom: "100px"
+        }}>
+
+          {drivers.map((driver, index) => (
+            <DriverInfoCard
+              name={driver.name}
+              rating={driver.rating}
+              distance={driver.distance}
+              eta={driver.eta}
+            />
+          ))}
+        </div>
       </div>
-      <br />
-      <MapContainer center={carPooler} zoom={14} style={{ height: '500px', width: '500px' }}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-      </MapContainer>
-    </div>
+    </>
   );
 }
 
